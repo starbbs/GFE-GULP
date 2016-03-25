301e7886522b6ff09af824fc55225e073febaf11
ccforeverd <zh1045456074@163.com>
2016-03-25 11:13:28 +0800

    从原项目中分离

---
+++ h5/h5-build.js
@@ -0,0 +1,151 @@
+
+// 张树垚 2015-12-10 14:59:58 创建
+// gulp H5微信端 生成到build文件夹
+
+
+var gulp = require('gulp');
+var path = require('path');
+
+var tools = require('../tools');
+var paths = require('./h5-paths');
+
+
+var notify = function(task, path) { // 提示
+	var notice = (path && !Array.isArray(path) && tools.filePath(path).type !== 'dir') ? tools.filePath(path).filename + ' ' : '全部文件';
+	return tools.notify(task + ': ' + notice + '编译完成!');
+};
+
+
+// html部分
+gulp.task('h5-include', function() {
+	var page = paths.pages + '/**/*.html';
+	var todo = function(path) {
+		return tools.fileInclude(path || page, paths.build, { strict: true })
+				.pipe(notify('h5-include', path));
+	};
+	gulp.watch(page, function(event) {
+		var path = tools.filePath(event.path);
+		return todo(path.path + '/' + path.dirname + '.' + path.extname);
+	});
+	gulp.watch([
+		path.join(paths.source, '/include/*.html'),
+		path.join(paths.views, '/**/*.html'),
+		path.join(paths.dialogs, '/**/*.html'),
+		path.join(paths.components, '/**/*.html')], function(event) {
+			return todo();
+		});
+	return todo();
+});
+
+
+// sass部分
+gulp.task('h5-sass', function() {
+	var page = path.join(paths.pages, '/**/*.scss');
+	var todo = function(url) {
+		return tools.sass(url || page, path.join(paths.build, '/css'))
+				.pipe(notify('h5-sass', url));
+	};
+	gulp.watch(page, function(event) {
+		return todo(event.path);
+	});
+	gulp.watch([
+		path.join(paths.source, '/scss/**/*.scss'),
+		path.join(paths.common, '/zSass/**/*.scss'),
+		path.join(paths.views, '/**/*.scss'),
+		path.join(paths.components, '/**/*.scss'),
+		path.join(paths.dialogs, '/**/*.scss')
+	], function(event) {
+		return todo();
+	});
+	return todo();
+});
+
+
+// 图片部分
+gulp.task('h5-img-move', function() {
+	var todo = function(url, useReg) {
+		var opts = {
+			type: 'image',
+			rename: function(url) {
+				url.dirname = url.dirname.replace(/\\/g, '/');
+				url.basename = url.dirname.replace(/(\/?images\/?)|(^\.)/, '') + url.basename.replace(/^_+/, '-');
+			}
+		};
+		useReg && (opts.fileReg = /^_/);
+		tools.fileMove(url, path.join(paths.build, '/images'), opts)
+			.pipe(notify('h5-img-move', url))
+	};
+	todo(path.join(paths.pages, '/**/**'), true);
+	todo(path.join(paths.source, '/images/**/**'));
+	gulp.watch([
+		path.join(paths.pages, '/**/**'), 
+		path.join(paths.source, '/images/**/**')
+	], function(event) {
+		var path = tools.filePath(event.path);
+		if (path.type === 'image') {
+			todo(path.origin, /\/pages\//.test(path.origin));
+		}
+	});
+});
+
+
+// js部分
+gulp.task('h5-js-move', function() {
+	var js = [path.join(paths.pages, '/**/*.js'), path.join(paths.common, '/library/base/*.js')];
+	var todo = function(url) {
+		return gulp.src(url)
+			.pipe(tools.removeDirname())
+			.pipe(gulp.dest(path.join(paths.build, '/js')))
+			.pipe(notify('h5-js-move', url))
+	};
+	gulp.watch(js, function(event) {
+		return todo(event.path);
+	});
+	return todo(js);
+	// var inputs = [
+	// 	path.join(paths.pages, '/**/*.js'), // 页面对应js
+	// 	path.join(paths.common, '/library/base/*.js') // 页面引入js
+	// ];
+	// return gulp.src(inputs, function() {
+	// 	arguments[1].forEach(function(input, index, array) {
+	// 		console.log(input);
+	// 	});
+	// });
+	// var todo = function(url) {
+	// 	return gulp.src(url)
+	// 		.pipe(tools.removeDirname())
+	// 		.pipe(gulp.dest(path.join(paths.build, '/js')))
+	// 		.pipe(notify('h5-js-move', url))
+	// };
+	// todo(js);
+	// gulp.watch(js, function(event) {
+	// 	return todo(event.path);
+	// });
+});
+
+
+// font部分
+gulp.task('h5-font-build', function() {
+	return gulp.src(path.join(paths.font, '*.*'))
+		.pipe(gulp.dest(path.join(paths.build, 'font')))
+});
+
+
+// template部分 -- 慎用, name不清会覆盖原文档
+gulp.task('h5-template', function() {
+	var date = new Date();
+	var opts = {
+		name: '', // name一定要清空, 慎用!
+		desc: '服务协议', // 描述'// H5微信端 --- {{desc}}'
+		date: date.toLocaleDateString().replace(/\//g, '-') + ' ' + date.toTimeString().split(' ')[0]
+	};
+	return tools.template('../h5/source/template/**', '../h5/factory/pages', opts).pipe(tools.notify(opts.name + '模版已建立完成!'));
+});
+
+
+gulp.task('h5-build', ['h5-include', 'h5-sass', 'h5-img-move', 'h5-js-move', 'h5-font-build']);
+
+
+
+
+
---
+++ src/01 notify 提示.js
@@ -0,0 +1,49 @@
+
+// 张树垚 2015-12-02 15:08:58 创建
+// gulp工具 -- 桌面提示
+
+
+// {
+// 	err: function(message) {},
+// 	success: function(message) {},
+// 	warning: function(message) {},
+// 	info: function(message) {},
+// }
+
+var notify = null;
+var images = {
+	avatar: ''
+};
+var maker = function(message, image) {
+	notify = notify || require('gulp-notify');
+	// if (!/zsy$/.test(message)) { message += ' -- by zsy'; }
+	return notify({
+		title: '果仁宝 by zsy', // 标题
+		// subtitle: 'subtitle', // 子标题(可省)
+		message: message,
+		// message: function(file) { // 内容, 默认文件路径
+		// 	return '' +
+		// 			// 'file.relative:' + file.relative + '\n' + // 文件名
+		// 			// 'file.contents:' + file.contents + // 文件内容
+		// 			'sucess:' + file.path + '\n' + // 文件路径
+		// 			// 'file.base:' + file.base + '\n' + // 文件夹路径
+		// 			// 'file.cwd:' + file.cwd + '\n' + // 相对路径
+		// 			'';
+		// },
+		sound: 'Glass', // 提示音: '叮'
+		// sound: 'Funk', // 提示音: '咚'
+		// sound: 'Ping', // 提示音: '噔'
+		// sound: 'Sosumi', // 提示音: '噌'
+		icon: './notify_images/h5.png', // 左侧小图
+		contentImage: './notify_images/avatar.png' // 右侧大图
+	});
+};
+var error = maker.error = function() {};
+var success = maker.success = function() {};
+
+
+module.exports = maker;
+
+
+
+
