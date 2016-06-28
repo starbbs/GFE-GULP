963043822a4578de8924d04514259454b46444b2
gaoerjun <gaoerjun@new4g.cn>
2016-06-28 09:17:39 +0800

    更正测试环境现有后台接口地址

--- h5/h5-online.js
+++ h5/h5-online.js
@@ -24,14 +24,19 @@ gulp.task('h5-rjs', ['h5-js'], function() {
 		rjsPaths: paths.rjs,
 		uglify: true,
 		isOnline:true,
-		oldChar: '172.16.33.3:18080',
+		//替换测试环境的后台接口为正式环境的域名
+		oldChar: '172.16.33.3:28080',
 		newChar: 'endpoint.goopal.com.cn',
+		//替换测试环境入口域名为正式环境的域名
 		oldChar1: 'www.xiaojian.me',
 		newChar1: 'www.goopal.com.cn',
+		//替换测试环境的微信appid为正式环境appid
 		oldChar2: 'wxe91980c4944999fe',
 		newChar2: 'wx55923db8dfb94e44',
+		//替换测试环境的统计地址为正式环境统计地址
 		oldChar3: '172.16.33.10:8089',
 		newChar3: 'track.goopal.com.cn',
+		//替换测试环境果仁市场地址为正式环境果仁市场地址
 		oldChar4:'exchange.xiaojian.me',
 		newChar4: 'www.goopal.net.cn'
 
